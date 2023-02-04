429f2cb69deba9b1742d302fc8fef8ceb0c8d644
Jeong Dong <eejxdong99@gmail.com>
2023-02-05 02:29:35 +0900

    feat: 상단 누적 시간, 거리 상태 창 추가

--- Netwalk-iOS/Controllers/PloggingViewController.swift
+++ Netwalk-iOS/Controllers/PloggingViewController.swift
@@ -11,32 +11,45 @@ import CoreLocation
 
 class PloggingViewController: UIViewController {
     
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var totalDistance: UILabel!
+    @IBOutlet weak var totalTime: UILabel!
+    
     var locationManager = CLLocationManager()
     var mapView: GMSMapView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        loadMapView()
+        setupView()
         setupLocation()
+    }
+    
+    func setupView() {
+        loadMapView()
+        topView.layer.cornerRadius = 10
     }
     
 
     func loadMapView() {
+        // 현재 내 위치 가져오기
+        print(#function)
         let myLocation = locationManager.location?.coordinate
         let latitude = myLocation?.latitude ?? 0.0
         let longitude = myLocation?.longitude ?? 0.0
         
         let camera = GMSCameraPosition(latitude: latitude, longitude: longitude, zoom: 18)
         mapView = GMSMapView(frame: self.view.bounds, camera: camera)
-        
+        mapView.delegate = self
         mapView.settings.myLocationButton = true
         mapView.isMyLocationEnabled = true
         
         view.addSubview(mapView)
+        view.bringSubviewToFront(topView)
     }
     
     func setupLocation() {
+        print(#function)
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest // 거리 정확도 설정
         locationManager.requestWhenInUseAuthorization() // 사용자에게 허용 받기 alert 띄우기
@@ -45,7 +58,6 @@ class PloggingViewController: UIViewController {
         DispatchQueue.global().async {
             if CLLocationManager.locationServicesEnabled() {
                 self.locationManager.startUpdatingLocation() // 위치 정보 받아오기 시작
-                print(self.locationManager.location?.coordinate)
             } else {
                 print("위치 서비스 Off 상태")
             }
@@ -54,9 +66,18 @@ class PloggingViewController: UIViewController {
 
 }
 
+// MARK: - GMSMapViewDelegate
+extension PloggingViewController: GMSMapViewDelegate {
+
+}
+
+// MARK: - CLLocationManagerDelegate
 extension PloggingViewController: CLLocationManagerDelegate {
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // 몇 초마다 호출되는지 체크해보기
         print(#function)
+        
         if let location = locations.first {
             print("위도: \(location.coordinate.latitude)")
             print("경도: \(location.coordinate.longitude)")
@@ -68,4 +89,5 @@ extension PloggingViewController: CLLocationManagerDelegate {
             
         }
     }
+
 }
