7ef36bd99c0480990ad87f0391265db20d4dc118
Jiyoon Kim <rlawldbs0911@naver.com>
2023-02-05 19:03:30 +0900

    feat: 스플래시 구현

--- Netwalk-iOS/AppDelegate.swift
+++ Netwalk-iOS/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+        sleep(1)
         GMSServices.provideAPIKey(Bundle.main.GOOGLE_API_KEY)
         GMSPlacesClient.provideAPIKey(Bundle.main.GOOGLE_API_KEY)
         FirebaseApp.configure()
