837be7aee28e2b58acfa794977cf6ff214afa96c
Jeong Dong <eejxdong99@gmail.com>
2023-02-10 23:27:00 +0900

    feat: 그룹 활성화 UI 수정

--- Netwalk-iOS/Views/Group/BodyCollectionViewCell.swift
+++ Netwalk-iOS/Views/Group/BodyCollectionViewCell.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class BodyCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var subView: UIView!
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var subName: UILabel!
+    @IBOutlet weak var groupTitle: UILabel!
+    @IBOutlet weak var personnel: UILabel!
+    @IBOutlet weak var statusColor: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        setupUI()
+    }
+    
+    func setupUI() {
+        containerView.layer.cornerRadius = 5
+        subView.layer.cornerRadius = 5
+        image.layer.cornerRadius = 5
+        statusColor.layer.cornerRadius = statusColor.frame.width / 2
+        
+        containerView.layer.borderWidth = 1
     }
 
 }
