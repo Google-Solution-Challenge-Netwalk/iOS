77c6500a37dec7be511513a51d335e7213d95a2a
Jeong Dong <eejxdong99@gmail.com>
2023-03-15 22:21:34 +0900

    feat: TabMan 적용

---
+++ Netwalk-iOS/Controllers/Ranking/TabManViewController.swift
@@ -0,0 +1,76 @@
+//
+//  TabManViewController.swift
+//  Netwalk-iOS
+//
+//  Created by 이정동 on 2023/03/15.
+//
+
+import UIKit
+import Tabman
+import Pageboy
+
+class TabManViewController: TabmanViewController {
+    
+    private var viewControllers: [UIViewController] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupTabMan()
+    }
+    
+    private func setupTabMan() {
+        
+        let timeRankVC = storyboard?.instantiateViewController(withIdentifier: "TimeRankViewController") as! TimeRankViewController
+        let kmRankVC = storyboard?.instantiateViewController(withIdentifier: "KmRankViewController") as! KmRankViewController
+        
+        viewControllers.append(timeRankVC)
+        viewControllers.append(kmRankVC)
+        
+        self.dataSource = self
+        let bar = TMBar.ButtonBar()
+        
+        bar.backgroundView.style = .blur(style: .light)
+        
+        bar.layout.contentInset = UIEdgeInsets(top: 0, left: 15, bottom: 0, right: 15)
+        
+        bar.buttons.customize { button in
+            button.tintColor = .systemGray4
+            button.selectedTintColor = .black
+            button.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+            button.selectedFont = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        }
+        
+        bar.indicator.weight = .custom(value: 2)
+        bar.indicator.tintColor = .black
+        addBar(bar, dataSource: self, at: .top)
+    }
+
+}
+
+extension TabManViewController: PageboyViewControllerDataSource, TMBarDataSource {
+    func numberOfViewControllers(in pageboyViewController: Pageboy.PageboyViewController) -> Int {
+        return viewControllers.count
+    }
+    
+    func viewController(for pageboyViewController: Pageboy.PageboyViewController, at index: Pageboy.PageboyViewController.PageIndex) -> UIViewController? {
+        return viewControllers[index]
+    }
+    
+    func defaultPage(for pageboyViewController: Pageboy.PageboyViewController) -> Pageboy.PageboyViewController.Page? {
+        return nil
+    }
+    
+    func barItem(for bar: Tabman.TMBar, at index: Int) -> Tabman.TMBarItemable {
+        switch index {
+        case 0:
+            return TMBarItem(title: "동네맛집")
+        case 1:
+            return TMBarItem(title: "겨울간식")
+        default:
+            return TMBarItem(title: "Test")
+        }
+    }
+    
+    
+}
---
+++ Netwalk-iOS/Controllers/Ranking/TimeRankViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TimeRankViewController.swift
+//  Netwalk-iOS
+//
+//  Created by 이정동 on 2023/03/15.
+//
+
+import UIKit
+
+class TimeRankViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
