06ac7947c2bdcc18068731839f7d506d720655f0
Jiyoon Kim <rlawldbs0911@naver.com>
2023-03-26 00:22:03 +0900

    feat: 그룹 생성 네트워킹 코드 구현

--- Netwalk-iOS/Networking/GroupNetManager.swift
+++ Netwalk-iOS/Networking/GroupNetManager.swift
@@ -11,6 +11,46 @@ class GroupNetManager {
     static let shared = GroupNetManager()
     private init() {}
     
+    // 그룹 생성
+    func createGroup (_ group: Group, completion: @escaping ()->()) {
+        
+        let urlKey = Bundle.main.getSecretKey(key: "REST_API_URL")
+        
+        guard let url = URL(string: "\(urlKey)/api/v1/group") else {
+            print("URL Error")
+            return
+        }
+        
+        guard let jsonData = try? JSONEncoder().encode(group) else {
+            print("Error: Trying to convert model to JSON data")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpBody = jsonData
+        
+        URLSession.shared.dataTask(with: request) { data, res, err in
+            
+            guard err == nil else {
+                print("Error: error calling POST")
+                print(err)
+                return
+            }
+
+            // HTTP 200번대 정상코드인 경우만 다음 코드로 넘어감
+            guard let response = res as? HTTPURLResponse, (200 ..< 299) ~= response.statusCode else {
+                print("Error: HTTP request failed")
+                return
+            }
+            
+            completion()
+            
+        }.resume()
+    }
+    
     // 카테고리별 그룹 보기
     func readCategoryGroup(_ category: String, completion: @escaping ([Group])->()) {
         let urlKey = Bundle.main.getSecretKey(key: "REST_API_URL")
